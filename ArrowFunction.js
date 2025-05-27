fc7100737d451b9a31b26ba9e438aa12ddc9a03a
C0ngHung <C0ngHungDCH@gmail.com>
2025-05-27 17:22:50 +0700

    ArrowFunction

---
+++ ArrowFunction.js
@@ -0,0 +1,61 @@
+// * 4. Arrow Function
+const logger = function(log) {
+    console.log(log);
+}
+logger('Message...'); 
+// * Chuyển đổi thành Arrow Function
+const loggerArrow = (log) => {
+    console.log(log);
+}
+
+// * Ví dụ 2.
+const sum = (a, b) => {
+    return a + b;
+}
+console.log(sum(2,2));
+// * Có thể viết gọn hơn như sau:
+const sum2 = (a, b) => a + b;
+console.log(sum2(3,3));
+// * Ví dụ 3. Với Object
+const sum3 = (a, b) => {
+    return {
+        a: a,
+        b: b
+    };
+};
+// * Có thể viết gọn hơn như sau:
+const sum4 = (a, b) => ({a: a, b:b});
+
+// * Ví dụ 4. Hàm nhận tham số duy nhất
+const loggger2 = (log) => console.log(log);
+// * Có thể việt gọn hơn như sau:
+const logger3 = log => console.log(log);
+// * Đặc tính của Arrow Function là không có từ khóa this, 
+// * do đó không thể sử dụng trong các phương thức của đối tượng. Ví dụ:
+const person = {
+    name: 'John',
+    age: 30,
+    greet: function() {
+        console.log(`Hello, my name is ${this.name}`);
+        // * this ở đây là context của đối tượng person
+    },
+    // * Không thể sử dụng Arrow Function ở đây
+    // * Arrow fuction không có context của nó: undefined
+    greetArrow: () => {
+        console.log(`Hello, my name is ${this.name}`); // this sẽ không trỏ đến person
+    }
+};
+// * Không thể sử dụng arrow function để làm function constructor được 
+// * Ví dụ cụ thể:
+const Course = function(name, price) {
+    this.name = name;
+    this.price = price
+}
+
+const jsCourse = new Course('JavaScript', 1000);
+console.log(jsCourse);
+// * Nhưng nếu đổi lại bằng Arrow funtion thì sẽ không được 
+const CourseArrow = (name, price) => {
+    this.name = name;
+    this.price = price;
+}; // * => TypeError: CourseArrow is not a constructor
